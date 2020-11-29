92f10dc23001c05fe0255581b266f3c4ded30912
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-29 14:57:34 +0100

    bo

--- install.sh
+++ install.sh
@@ -6,6 +6,23 @@
 # /dev/sda1    EFI System                      500MiB
 # /dev/sda2    Arch-Linux                      Remaining Space
 
+# Partitioning
+efi_partition=/dev/sda1
+efi_partition_size=512M
+
+linux_partition=/dev/sda2
+
+# File System Structure
+efi_directory=/boot/efi
+bootloader_id=GRUB
+target=x86_64-efi
+
+keymap=it
+locale='en_US.UTF-8 UTF-8'
+lang='en_US.UTF-8'
+region=Europe
+location=Rome
+hostname=Matebook
 
 packages=(
     base 
@@ -20,34 +37,37 @@ packages=(
 )
 
 
-loadkeys it
+loadkeys ${keymap}
 timedatectl set-ntp true
 
 # Partition
 # Format
-# Mount
 
-pacstrap /mnt  packages[@]
+mount ${linux_partition} /mnt
+mkdir -p /mnt${efi_directory}
+mount ${efi_partition} /mnt${efi_directory}
+
+pacstrap /mnt  ${packages[@]}
 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+ln -sf /usr/share/zoneinfo/${region}/${location} /etc/localtime
 hwclock --systohc
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+echo ${locale} >> /etc/locale.gen
 locale-gen
-echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
-echo 'KEYMAP=it' >> /etc/vconsole.conf
+echo 'LANG=${lang}' >> /etc/locale.conf
+echo 'KEYMAP=${key}' >> /etc/vconsole.conf
 
-echo 'Matebook' > /etc/hostname
+echo '${hostname}' > /etc/hostname
 
 echo "
 127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   Matebook.localdomain Matebook   
+127.0.1.1   ${hostname}.localdomain ${hostname}   
 " >> /etc/hosts
 
 mkinitcpio -P
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=${target} --efi-directory=${efi_directory} --bootloader-id=${bootloader_id}
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Set passwd for root
