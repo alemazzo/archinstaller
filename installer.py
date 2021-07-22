03bddced7412dd89d4deb9d8380e5c7cc108cbd3
Alessandro Mazzoli <alemazzoli97@gmail.com>
2021-07-22 12:23:00 +0200

    added new feature

--- installer.py
+++ installer.py
@@ -25,23 +25,48 @@ class STATUS:
     ERROR = f'[{colors.FAIL}\u2714{colors.ENDC}]'
 
 
-def execute(command: str):
-    message = f'{STATUS.PROGRESS} {command}'
+def execute(command: str, description: str):
+    message = f'{STATUS.PROGRESS} {description}'
     print(message, end='', flush=True)
     process = subprocess.Popen(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     process.wait()
     if process.returncode == 0:
-        message = f'{STATUS.OK} {command}'
+        message = f'{STATUS.OK} {description}'
     else:
-        message = f'{STATUS.ERROR} {command}'
+        message = f'{STATUS.ERROR} {description}'
     print(f'\r{message}')
 
 
 def setKeyMap(keyMap: str):
-    print(f'Settings keymap to {keymap}')
+    description = f'Settings keymap to {keymap}'
     command = f'loadkeys {keymap}'
-    execute(command)
+    execute(command, description)
+
+
+def setTimeProtocol(ntp: bool):
+    description = f'Settings NTP to {ntp}'
+    command = f'timedatectl set-ntp {ntp}'
+    execute(command, description)
+
+
+def mountLinuxPartition(linux_partition: str):
+    description = f'Mounting Linux Partition from {linux_partition}'
+    command = f'mount {linux_partition} /mnt'
+    execute(command, description)
+
+
+def mountEfiPartition(efi_partition: str, path: str):
+    description = f'Mounting Efi Partition from {linux_partition} to {path}'
+    command = f'mkdir -p /mnt{path}; mount {efi_partition} /mnt{path}'
+    execute(command, description)
+
+
+def installPackage(package: str):
+    description = f'Installing {package}'
+    command = f'pacstrap /mnt {package}'
+    execute(command, description)
+
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -53,6 +78,21 @@ if __name__ == "__main__":
     settings = data['settings']
     packages = data['packages']
 
+    # Set keymap
     keymap = settings['keymap']
+    setKeyMap(keymap)
 
-    print(f'Packages = {packages}')
+    # Set time protocol
+    ntp = settings['ntp']
+    setTimeProtocol(ntp)
+
+    # Mounting partitions
+    linux_partition = partitions['linux']
+    efi_partition = partitions['efi']
+    efi_directory = boot['efi_directory']
+    mountLinuxPartition(linux_partition)
+    mountEfiPartition(efi_partition, efi_directory)
+
+    # Installing packages
+    for package in packages:
+        installPackage(package)
