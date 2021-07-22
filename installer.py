031e4a79ae260d18e3185091492a307a915761b1
Alessandro Mazzoli <alemazzoli97@gmail.com>
2021-07-22 12:29:16 +0200

    enable error log

--- installer.py
+++ installer.py
@@ -29,7 +29,7 @@ def execute(command: str, description: str):
     message = f'{STATUS.PROGRESS} {description}'
     print(message, end='', flush=True)
     process = subprocess.Popen(
-        command, shell=True, stdout=subprocess.PIPE)
+        command, shell=True, stdout=subprocess.STDOUT)
     process.wait()
     if process.returncode == 0:
         message = f'{STATUS.OK} {description}'
