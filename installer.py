09fdbd5c8659703be3d2486e578b9826b4fc56b5
Alessandro Mazzoli <alemazzoli97@gmail.com>
2021-07-23 09:03:22 +0200

    Integreted chroot script with command line parameter for switching

--- installer.py
+++ installer.py
@@ -38,7 +38,7 @@ def execute(command: str, description: str):
     print(f'\r{message}')
 
 
-def setKeyMap(keyMap: str):
+def setKeyMap(keymap: str):
     description = f'Settings keymap to {keymap}'
     command = f'loadkeys {keymap}'
     execute(command, description)
@@ -57,7 +57,7 @@ def mountLinuxPartition(linux_partition: str):
 
 
 def mountEfiPartition(efi_partition: str, path: str):
-    description = f'Mounting Efi Partition from {linux_partition} to {path}'
+    description = f'Mounting Efi Partition from {efi_partition} to {path}'
     command = f'mkdir -p /mnt{path}; mount {efi_partition} /mnt{path}'
     execute(command, description)
 
@@ -74,11 +74,19 @@ def generateFstab():
     execute(command, description)
 
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    configurationFilePath = args.file
-    data = loadYamlFromFile(configurationFilePath)
+def copyInstallerInMNT():
+    description = 'Copying installer to /mnt'
+    command = 'cp -r ./ /mnt'
+    execute(command, description)
 
+
+def chrootAndExecute():
+    description = 'Running chroot'
+    command = 'arch-chroot /mnt /archiso/installer.py --chroot'
+    execute(command, description)
+
+
+def archiso(data):
     partitions = data['partitions']
     boot = data['boot']
     settings = data['settings']
@@ -105,3 +113,172 @@ if __name__ == "__main__":
 
     # Generate fstab
     generateFstab()
+
+    # Copying installer to /mnt
+    copyInstallerInMNT()
+
+    # Chroot and execute
+    chrootAndExecute()
+
+
+def linkTime(region: str, location: str):
+    description = f'Linking time to {location}/{region}'
+    command = f'ln -sf /usr/share/zoneinfo/{region}/{location} /etc/localtime'
+    execute(command, description)
+
+
+def setHwClock():
+    description = 'Setting hwclock'
+    command = 'hwclock --systohc'
+    execute(command, description)
+
+
+def addLocale(locale: str):
+    description = f'Adding locale {locale}'
+    command = f'echo {locale} >> /etc/locale.gen; locale-gen'
+    execute(command, description)
+
+
+def addLang(lang: str):
+    description = f'Adding lang {lang}'
+    command = f'echo "LANG={lang}" >> /etc/locale.conf'
+    execute(command, description)
+
+
+def addKeyMap(keymap: str):
+    description = f'Adding keymap {keymap}'
+    command = f'echo "KEYMAP={keymap}" >> /etc/vconsole.conf'
+    execute(command, description)
+
+
+def addHostname(hostname: str):
+    description = f'Adding hostname {hostname}'
+    command = f'echo "{hostname}" > /etc/hostname'
+    execute(command, description)
+
+
+def setupHosts():
+    description = 'Setting up hosts'
+    command = 'echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts'
+    execute(command, description)
+
+
+def mkinit():
+    description = 'mkinit'
+    command = 'mkinitcpio -P'
+    execute(command, description)
+
+
+def grubInstall(target, efi_directory, bootloader_id):
+    description = f'Installing GRUB to {target}'
+    command = f'grub-install --target={target} --efi-directory={efi_directory} --bootloader-id={bootloader_id}'
+    execute(command, description)
+
+
+def mkconfig():
+    description = 'mkconfig'
+    command = 'grub-mkconfig -o /boot/grub/grub.cfg'
+    execute(command, description)
+
+
+def setPasswForRoot(password: str):
+    description = f'Setting root password to {password}'
+    command = f'echo "{password}" | passwd root'
+    execute(command, description)
+
+
+def createUser(username: str, password: str):
+    description = f'Creating user {username} with password {password}'
+    command = f'useradd -m -G wheel -s /bin/bash -p {password} {username}'
+    execute(command, description)
+
+
+def addWheelToSudoers():
+    description = 'Adding wheel group to sudoers'
+    command = 'echo "%wheel ALL=(ALL) ALL" | EDITOR="tee -a" visudo'
+    execute(command, description)
+
+
+def enableSddm():
+    description = 'Enabling sddm'
+    command = 'systemctl enable sddm.service'
+    execute(command, description)
+
+
+def enableNetworkManager():
+    description = 'Enabling NetworkManager'
+    command = 'systemctl enable NetworkManager.service'
+    execute(command, description)
+
+
+def chroot(data):
+    partitions = data['partitions']
+    boot = data['boot']
+    settings = data['settings']
+    packages = data['packages']
+    accounts = data['accounts']
+
+    # Link time
+    region = settings['region']
+    location = settings['location']
+    linkTime(region, location)
+
+    # Set hwclock
+    setHwClock()
+
+    # Add locale
+    locale = settings['locale']
+    addLocale(locale)
+
+    # Add lang
+    lang = settings['lang']
+    addLang(lang)
+
+    # Add keymap
+    keymap = settings['keymap']
+    addKeyMap(keymap)
+
+    # Add hostname
+    hostname = settings['hostname']
+    addHostname(hostname)
+
+    # Setup hosts
+    setupHosts()
+
+    # Grub install
+    target = boot['target']
+    efi_directory = boot['efi-directory']
+    bootloader_id = boot['bootloader-id']
+    grubInstall(target, efi_directory, bootloader_id)
+
+    # mkconfig
+    mkconfig()
+
+    # Set root password
+    password = accounts['root-password']
+    setPasswForRoot(password)
+
+    # Create user
+    username = accounts['username']
+    password = accounts['password']
+    createUser(username, password)
+
+    # Add wheel to sudoers
+    addWheelToSudoers()
+
+    # Enable sddm
+    enableSddm()
+
+    # Enable NetworkManager
+    enableNetworkManager()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    configurationFilePath = args.file
+    data = loadYamlFromFile(configurationFilePath)
+
+    if args.chroot:
+        chroot(data)
+    else:
+        archiso(data)
--- modules/arguments/parser.py
+++ modules/arguments/parser.py
@@ -9,20 +9,8 @@ def get_argument_parser():
     parser.add_argument('file',
                         help='YAML configuration file')
 
-    parser.add_argument('--tasks',  action="store_true",
-                        help='Print available tasks')
-
-    parser.add_argument('--apptypes',  action="store_true",
-                        help='Print available application types')
-
-    parser.add_argument('-t', '--task',
-                        help='Task to execute ([all] for executing all tasks)')
-
-    parser.add_argument('-at', '--apptype',
-                        help='Application type ([all] for all application types)')
-
-    parser.add_argument('-a', '--app',
-                        help='Application to install ([all] for executing all application types)')
+    parser.add_argument('--chroot',  action="store_true",
+                        help='Execute the chroot install')
 
     return parser
 
